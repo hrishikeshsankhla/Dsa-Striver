e103b0d6819d0a09906ecfca0665a8f075db4bf9
Hrishikesh <136251498+hrishikeshsankhla@users.noreply.github.com>
2024-08-05 07:22:12 +0530

    Time: 0 ms (100%), Space: 8.3 MB (35.24%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double myPow(double x, int n) 
+    {
+        if(n == 0) {
+            return 1;
+        }
+        
+        // Handling negative exponent
+        long long N = n;  // Use long long to handle cases where n is INT_MIN
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        return power(x, N);
+    }
+    
+private:
+    double power(double x, long long n) {
+        if(n == 0) {
+            return 1;
+        }
+        double half = power(x, n / 2);
+        if(n % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+};
